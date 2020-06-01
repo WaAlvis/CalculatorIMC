4f99363024c8a555ea7db4229a4ad0d4a2307503
William Alvis <alvis.wap@gmail.com>
2020-06-01 11:48:58 -0500

    extayendo nuevos widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'custon_widgets/icon_content.dart';
+import 'custon_widgets/reusable_card.dart';
 
 const bottomContainerHeight = 70.0;
 const activeCardColour = Color(0xFF1D1E33);
@@ -10,6 +14,7 @@ class InputPage extends StatefulWidget {
 }
 
 //1DIE33
+//8D8E98
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
@@ -23,10 +28,19 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(colour: activeCardColour),
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
+                  ),
                 ),
                 Expanded(
-                  child: ReusableCard(colour: activeCardColour),
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE',),
+                  ),
                 ),
               ],
             ),
@@ -57,19 +71,4 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class ReusableCard extends StatelessWidget {
-  Color colour;
 
-  ReusableCard({@required this.colour});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
-  }
-}
