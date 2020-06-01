40a80bf546530a702964187b19969592a27283de
William Alvis <alvis.wap@gmail.com>
2020-06-01 07:37:56 -0500

    reutilizando card

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 70.0;
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
+//1DIE33
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
@@ -12,10 +17,58 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('Calculadora IMC'),
       ),
-      body: Center(
-        child: Text(
-          'body Text',
-        ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: activeCardColour),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: activeCardColour),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(colour: activeCardColour),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: activeCardColour),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: activeCardColour),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: bottomContainerHeight,
+            color: bottomContainerColour,
+            width: double.infinity,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  Color colour;
+
+  ReusableCard({@required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
